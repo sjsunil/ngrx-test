b4bc6519740c97ae53fc361a2dfeca46697ef8e3
Sunil Jena <suniljena@Sunils-MacBook-Air.local>
2025-09-26 18:58:48 +0530

    First Commit

---
+++ src/app/pages/admin/admin.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from '../../store/store';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-admin',
+  imports: [CommonModule],
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.scss',
+})
+export class AdminComponent {
+  count!: Observable<number>;
+
+  constructor(private store: Store<AppState>) {
+    this.count = this.store.pipe(select('count'));
+  }
+}
